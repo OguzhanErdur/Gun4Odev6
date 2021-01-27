9e1149e3eb1a10ff0bf544fabd9cf32a7e291624
Oğuzhan ERDUR <Ouzhan ERDUR@ERDUR>
2021-01-27 06:17:38 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gun4Odev6
+{
+    class MyDictionary<Name,Number>
+    {
+        Number[] numbers;
+        Name[] names;
+
+        Number[] tempNumbers;
+        Name[] tempNames;
+
+        public MyDictionary()
+        {
+            numbers = new Number[0];
+            names = new Name[0];
+
+        }
+
+        public void Add(Name sName, Number sNumber)
+        {
+            tempNumbers = numbers;
+            tempNames = names;
+
+            names = new Name[names.Length + 1];
+            numbers = new Number[numbers.Length + 1];
+
+            for (int i = 0; i < tempNames.Length; i++)
+            {
+                names[i] = tempNames[i];
+            }
+            names[names.Length - 1] = sName;
+
+            for (int i = 0; i < tempNumbers.Length; i++)
+            {
+                numbers[i] = tempNumbers[i];
+            }
+            numbers[numbers.Length - 1] = sNumber;
+
+            Console.WriteLine(sName + " "+ sNumber + " eklendi!");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Gun4Odev6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> student = new MyDictionary<string, int>();
+
+            student.Add("Ahmet",123);
+            student.Add("Mehmet",321);
+            student.Add("Kamil", 789);
+
+         
+        }
+    }
+}
